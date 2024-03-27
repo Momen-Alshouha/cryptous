e01eada40bbc0aa86b5d175c94c668b548c74000
momen-alshouha <momenalshouha@outlook.com>
2024-03-27 22:37:47 +0300

    download app section done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CryptoCurrencyPricesComponent } from './crypto-currency-prices/crypto-currency-prices.component';
 import { FeaturesComponent } from './features/features.component';
+import { environment } from '../../environments/environment';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CryptoCurrencyPricesComponent,FeaturesComponent],
+  imports: [CryptoCurrencyPricesComponent, FeaturesComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-
+  protected environment = environment;
 }
